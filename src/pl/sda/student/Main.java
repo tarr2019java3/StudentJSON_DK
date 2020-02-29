b2052015153648f84210dce39fadd932264e9e95
Daniel <kapron.daniel@gmail.com>
2020-02-29 12:04:31 +0100

    dodanie studentow i listy

--- src/pl/sda/student/Main.java
+++ src/pl/sda/student/Main.java
@@ -4,8 +4,13 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
+
+    // !!! serializacja - przeksztalcenie obiektu na forme np. tekstową !!!
+    // będziemy serializować ten obiekt do JSON'a
 
     public static void main(String[] args) {
 
@@ -13,18 +18,48 @@ public class Main {
                 "Kowalski",
                 32);
 
-        // !!! serializacja - przeksztalcenie obiektu na forme np. tekstową !!!
-        // będziemy serializować ten obiekt do JSON'a
+        Student student1 = new Student("Andrzej",
+                "Kowalski",
+                15);
+
+        Student student2 = new Student("Janusz",
+                "Kowalski",
+                25);
+
+        Student student3 = new Student("Janek",
+                "Kowalski",
+                30);
+
+        Student student4 = new Student("Zbyszek",
+                "Kowalski",
+                22);
+
+        Student student5 = new Student("Alex",
+                "Kowalski",
+                55);
+
+        List<Student> studentLista = new ArrayList<>();
+        studentLista.add(student);
+        studentLista.add(student1);
+        studentLista.add(student2);
+        studentLista.add(student3);
+        studentLista.add(student4);
+        studentLista.add(student5);
+
 
         ObjectMapper objectMapper = new ObjectMapper();
 
-
         try {
             objectMapper.writeValue(new File("student.json"), student);
+            objectMapper.writeValue(new File("student1.json"), student1);
+            objectMapper.writeValue(new File("student2.json"), student2);
+            objectMapper.writeValue(new File("student3.json"), student3);
+            objectMapper.writeValue(new File("student4.json"), student4);
+            objectMapper.writeValue(new File("student5.json"), student5);
+            objectMapper.writeValue(new File("studentLista.json"), studentLista);
         } catch (IOException e){
             e.printStackTrace();
         }
-
 
     }
 
